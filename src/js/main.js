77e89d7f8f7089ade4153f6b64e89df618363318
darek vassalli <98399869+DarekV@users.noreply.github.com>
2023-05-04 10:17:58 +0200

    début swiper

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,17 @@ import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 1,
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    bulletClass: "swiper-pagination-bullet",
+  },
+  breakpoints: {
+    575: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
